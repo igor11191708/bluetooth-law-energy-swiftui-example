697eae0f50e47313d23ad48c9748aa470098c87a
Igor <igorshelopaev@gmail.com>
2024-07-12 19:10:26 +0200

    Initial Commit

---
+++ bluetooth-law-energy_example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bluetooth-law-energy_example
+//
+//  Created by Igor  on 12.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bluetooth-law-energy_example/bluetooth_law_energy_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluetooth_law_energy_exampleApp.swift
+//  bluetooth-law-energy_example
+//
+//  Created by Igor  on 12.07.24.
+//
+
+import SwiftUI
+
+@main
+struct bluetooth_law_energy_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
